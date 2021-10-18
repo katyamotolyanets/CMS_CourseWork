9e9a110acb780de5933defe557b3667c284e00ca
katya <katiamotolianec@gmail.com>
2021-10-18 12:44:33 +0300

    add component Law to TheoryPage

--- src/components/theory/Law.js
+++ src/components/theory/Law.js
@@ -1,24 +1,48 @@
 import ReactFancyBox from "react-fancybox";
 import React from "react";
+import '../../App.scss';
 
 function Law(props) {
-    return (
+    if (props.isFancy) {
+        return (
         <div className="law" id={props.id}>
             <div className="header">
                 <p>{props.header}</p>
             </div>
-            <div className="description">
+            <div className="description" >
                 <div className="poster-img">
                     <ReactFancyBox image={props.img}
                                    showCloseBtn={false}>
                     </ReactFancyBox>
                 </div>
-                <div className="info">
-                    <p>{props.info}</p>
+                <div className="info" style={{marginLeft: 0}}>
+                    <p>{props.first}</p>
+                    <p>{props.second}</p>
+                    <p>{props.third}</p>
+                    <a className="formula">{props.formula}</a>
+                    <div className="formula-div">
+                        <img src={props.imgF} alt="Формула"/>
+                    </div>
                 </div>
             </div>
         </div>
-    )
+    ) }
+    else {
+        return (
+            <div className="law" id={props.id}>
+                <div className="header">
+                    <p>{props.header}</p>
+                </div>
+                <div className="description" >
+                    <div className="info" style={{marginLeft: 0}}>
+                        <p>{props.first}</p>
+                        <p>{props.second}</p>
+                        <p>{props.third}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Law;
